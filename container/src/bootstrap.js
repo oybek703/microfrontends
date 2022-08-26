7b7b0930e61a3d3c5957475fb7015db873fc5264
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-26 09:32:14 +0500

    starting new project

--- container/src/bootstrap.js
+++
@@ -1,7 +0,0 @@
-import {mount as productsMount} from 'products/ProductList'
-import {mount as cartMount} from 'cart/CartShow'
-
-console.log('Container!')
-
-productsMount(document.getElementById('my-products'))
-cartMount(document.getElementById('my-cart'))
