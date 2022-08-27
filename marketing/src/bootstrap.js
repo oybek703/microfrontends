e8c06d52300b612c84867bbe89f3bee3ad88b5f0
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 22:33:26 +0500

    check mount in marketing

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 
 function mount(el) {
+  console.log(el)
+  console.log('check is mount working here.')
   ReactDOM.render(<App/>, el)
 }
 
