9e8987f1a7b515ea711fdc78cdbf787f4c1c53b7
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 18:39:06 +0500

    check gh-pages

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,7 +3,8 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
-const domain = process.env.PRODUCTION_DOMAIN
+const domain = 'http://oybek703.github.io/microfrontends'
+// const domain = process.env.PRODUCTION_DOMAIN
 
 const devConfig = {
   mode: 'production',
