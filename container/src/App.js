8d38dd22a6dff91d2a1f7a6962696dee02689e1c
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 22:09:30 +0500

    header added

--- container/src/App.js
+++ container/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
+import {BrowserRouter} from 'react-router-dom'
 
 
 const App = () => {
   return (
-      <div>
+      <BrowserRouter>
         <h1>Hi there.</h1>
         <hr/>
+        <Header/>
         <MarketingApp/>
-      </div>
+      </BrowserRouter>
   )
 }
 
