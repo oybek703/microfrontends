d3a894d765680db946c586ecbfb2e9b7287d98a0
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-28 00:38:35 +0500

    d5

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,6 @@ import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 import {BrowserRouter} from 'react-router-dom'
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles'
-import {mount} from 'marketing/MarketingApp'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
@@ -16,8 +15,7 @@ const App = () => {
           <h1>Hi there.</h1>
           <hr/>
           <Header/>
-          {mount(document.getElementById('marketing_root'))}
-          {/*<MarketingApp/>*/}
+          <MarketingApp/>
         </BrowserRouter>
       </StylesProvider>
   )
