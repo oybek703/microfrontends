ee838a99d463f9032b6aaae9ffd6abe79b056b33
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 23:00:10 +0500

    check mount log

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -7,9 +7,8 @@ const MarketingApp = () => {
     console.log(ref.current)
     console.log(mount)
     console.log(typeof mount)
-    console.log(mount(ref))
-    console.log(mount())
-    mount(ref.current)
+    console.log(mount(ref.current))
+    // mount(ref.current)
   })
   return (
       <div ref={ref}/>
