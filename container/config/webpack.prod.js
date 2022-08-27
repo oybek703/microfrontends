2c17242d5e3c8a8b7fc7f6efbe2bd989034357d3
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 18:46:50 +0500

    https added

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
 
-const domain = 'http://oybek703.github.io/microfrontends'
+const domain = 'https://oybek703.github.io/microfrontends'
 // const domain = process.env.PRODUCTION_DOMAIN
 
 const devConfig = {
