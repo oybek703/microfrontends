f98e81f7193276b02d6c0410e422ef3f1719f2ce
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 17:54:31 +0500

    deploy added

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,8 +1,7 @@
 const {merge} = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
-// const packageJson = require('../package.json')
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
@@ -19,11 +18,7 @@ const devConfig = {
         exposes: {
           './MarketingApp': './src/bootstrap.js'
         },
-        // shared: packageJson.dependencies
-        // shared: ['react', 'react-dom']
-      }),
-      new HtmlWebpackPlugin({
-        template: './public/index.html'
+        shared: packageJson.dependencies
       })
   ]
 }
