71e36853eb7c66ca7a622a47a55244562a06f4c1
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-27 21:57:52 +0500

    log ref current

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -3,6 +3,7 @@ import {mount} from 'marketing/MarketingApp'
 
 const MarketingApp = () => {
   const ref = useRef(null)
+  console.log(ref.current)
   useEffect(() => {
     mount(ref.current)
   })
