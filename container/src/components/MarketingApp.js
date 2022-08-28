5c83422762defa1ff4cc2979f94949357c06aa28
Yoriqulov Oybek <hhoybek@gmail.com>
2022-08-28 12:23:02 +0500

    d22

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -4,9 +4,7 @@ import {mount} from 'marketing/MarketingApp'
 const MarketingApp = () => {
   const ref = useRef(null)
   useEffect(() => {
-    console.log('BEFORE => ', ref.current)
     mount(ref.current)
-    console.log('AFTER => ', ref.current)
   })
   return <div ref={ref}/>
 }
